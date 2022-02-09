e53cd4ce6fd7a4561d489b1b88e0c1bfc603a433
Filip Dusil <dusilf.05@spst.eu>
2022-02-09 09:36:51 +0100

    morzeovka 0-5

--- P02/Form1.cs
+++ P02/Form1.cs
@@ -16,5 +16,35 @@ namespace P02
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ----- ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " .---- ";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ..--- ";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ...-- ";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ....- ";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ..... ";
+        }
     }
 }
